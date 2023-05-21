55db55923d4ce995719c2b3684022f865839b13a
Ed <mail@544062.ru>
2023-05-21 18:20:47 +0300

    refactor: changed Loader component

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,10 +1,18 @@
 import { Loader as LoaderApp } from "@mantine/core";
 import "./Loader.css";
 
-export default function Loader() {
+interface Props {
+  children?: React.ReactNode;
+}
+const Loader: React.FC<Props> = ({ children }) => {
   return (
+    <>
     <div className="loader">
       <LoaderApp size="xl" variant="dots" />
+      {children && (<h4>{children}</h4>)}
     </div>
+    </>
   );
 }
+
+export default Loader;
