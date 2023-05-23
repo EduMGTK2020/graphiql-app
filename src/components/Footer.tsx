0768e31469ed98a5b1d1e592b393dcd8f5df9f13
Farkhod Kodirov <farxod94@gmail.com>
2023-05-24 01:38:55 +0500

    feat: fill footer data

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,22 @@
-import { useTranslation } from "react-i18next";
+// import { useTranslation } from "react-i18next";
 import "./Footer.css";
 
 export default function Footer() {
-  const { t } = useTranslation();
+  // const { t } = useTranslation();
 
   return (
     <>
       <footer className="footer_app">
-        <h2>{t("footerTitle")}</h2>
-        <p>{t("footerInfo")}</p>
+        <div className="footer_githubs">
+          <img src="/github.svg" />{" "}
+          <a href="https://github.com/edumgtk2020">edumgtk2020</a> |{" "}
+          <a href="https://github.com/Clukva">Clukva</a> |{" "}
+          <a href="https://github.com/fkodirov">fkodirov</a>
+        </div>
+        <p className="footer_year">2023</p>
+        <a href="https://rs.school/react/" className="footer_logo">
+          <img src="/rs-school.svg" alt="RS School" height={28} />
+        </a>
       </footer>
     </>
   );
